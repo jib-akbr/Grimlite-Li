f991d5465215a861264d7165d9008bb45519a5ae
letsssgoo <164711032+letsssgoo@users.noreply.github.com>
2024-03-24 11:41:19 +0700

    add statements player in my cell greater/less than

---
+++ Grimoire/Botting/Commands/Misc/Statements/CmdPlayerInMyCellLessThan.cs
@@ -0,0 +1,47 @@
+using Grimoire.Game;
+using System.Collections.Generic;
+using System;
+using System.Threading.Tasks;
+using Grimoire.Tools;
+using Newtonsoft.Json.Linq;
+
+namespace Grimoire.Botting.Commands.Misc.Statements
+{
+    public class CmdPlayerInMyCellLessThan : StatementCommand, IBotCommand
+    {
+        public CmdPlayerInMyCellLessThan()
+        {
+            Tag = "Player";
+            Text = "Player count is less than [in my cell]";
+        }
+
+        public Task Execute(IBotEngine instance)
+        {
+            int count = 0;
+            List<string> playerInMap = World.PlayersInMap;
+            foreach (string pl in playerInMap)
+            {
+                string reqs = Flash.Call<string>("CheckCellPlayer", new string[] {
+                    pl,
+                    Player.Cell
+                });
+                if (bool.Parse(reqs))
+                {
+                    count++;
+                }
+            }
+
+            if (count >= int.Parse((instance.IsVar(Value1) ? Configuration.Tempvariable[instance.GetVar(Value1)] : Value1)))
+            {
+                instance.Index++;
+            }
+            return Task.FromResult<object>(null);
+        }
+
+        public override string ToString()
+        {
+            return "Player count is less than [in my cell]: " + Value1;
+        }
+    }
+}
+
