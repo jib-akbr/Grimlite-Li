7c48eb12078bf13239ee4e81f0deb87b6147317b
Vicinity4519 <162864584+Vicinity4519@users.noreply.github.com>
2024-08-03 17:47:36 +0000

    Fix syntax

--- Grimoire/Botting/Commands/Misc/Statements/CmdNameEquals.cs
+++ Grimoire/Botting/Commands/Misc/Statements/CmdNameEquals.cs
@@ -14,7 +14,7 @@ namespace Grimoire.Botting.Commands.Misc.Statements
 
         public Task Execute(IBotEngine instance)
         {
-            if (Player.Username.Equals(instance.IsVar(Value1) ? Configuration.Tempvariable[instance.GetVar(Value1)] : Value1, StringComparison.OrdinalIgnoreCase)
+            if (Player.Username.Equals(instance.IsVar(Value1) ? Configuration.Tempvariable[instance.GetVar(Value1)] : Value1, StringComparison.OrdinalIgnoreCase))
             {
                 instance.Index++;
             }
