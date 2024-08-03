3122b3177b24e9071b7d1cf2f5260d08ca45afbb
Vicinity4519 <162864584+Vicinity4519@users.noreply.github.com>
2024-08-03 17:29:16 +0000

    Add 'This Player Name Equals' Statement

---
+++ Grimoire/Botting/Commands/Misc/Statements/CmdNameEquals.cs
@@ -0,0 +1,29 @@
+using Grimoire.Game;
+using System;
+using System.Threading.Tasks;
+
+namespace Grimoire.Botting.Commands.Misc.Statements
+{
+    public class CmdNameEquals : StatementCommand, IBotCommand
+    {
+        public CmdNameEquals()
+        {
+            Tag = "This player";
+            Text = "Name Equals";
+        }
+
+        public Task Execute(IBotEngine instance)
+        {
+            if (Player.Username.Equals(instance.IsVar(Value1) ? Configuration.Tempvariable[instance.GetVar(Value1)] : Value1, StringComparison.OrdinalIgnoreCase)
+            {
+                instance.Index++;
+            }
+            return Task.FromResult<object>(null);
+        }
+
+        public override string ToString()
+        {
+            return "Name Equals " + Value1;
+        }
+    }
+}
