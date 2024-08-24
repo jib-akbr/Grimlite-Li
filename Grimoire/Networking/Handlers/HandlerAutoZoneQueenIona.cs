30c14b0264f53bcb5196b6731a83a69d6c4e13e2
Vicinity4519 <162864584+Vicinity4519@users.noreply.github.com>
2024-08-23 20:16:11 -0700

    Update HandlerAutoZoneQueenIona.cs

--- Grimoire/Networking/Handlers/HandlerAutoZoneQueenIona.cs
+++ Grimoire/Networking/Handlers/HandlerAutoZoneQueenIona.cs
@@ -3,30 +3,47 @@ using Newtonsoft.Json.Linq;
 
 namespace Grimoire.Networking.Handlers
 {
-	public class HandlerAutoZoneQueenIona: IJsonMessageHandler
-	{
-		public string[] HandledCommands { get; } = { "event" };
+    public class HandlerAutoZoneQueenIona : IJsonMessageHandler
+    {
+        public string[] HandledCommands { get; } = { "event" };
 
-		public void Handle(JsonMessage message)
-		{
-			try
-			{
-				JObject args = (JObject)message.DataObject["args"];
-				string zone = args["zoneSet"].ToString();
-				switch (zone)
-				{
-					case "A":
-						Player.WalkToPoint("217", "366");
-						break;
-					case "B":
-						Player.WalkToPoint("867", "308");
-						break;
-					default:
-						Player.WalkToPoint("485", "351");
-						break;
-				}
-			}
-			catch { }
-		}
-	}
+        public void Handle(JsonMessage message)
+        {
+            try
+            {
+                JObject args = (JObject)message.DataObject["args"];
+                string zone = args["zoneSet"].ToString();
+                int positiveChargeCount = Player.GetAuras(true, "Positive Charge");
+                int negativeChargeCount = Player.GetAuras(true, "Negative Charge");
+
+                switch (zone)
+                {
+                    case "A":
+                        if (positiveChargeCount > 0)
+                        {
+                            Player.WalkToPoint("678", "310");
+                        }
+                        else
+                        {
+                            Player.WalkToPoint("217", "366");
+                        }
+                        break;
+                    case "B":
+                        if (negativeChargeCount > 0)
+                        {
+                            Player.WalkToPoint("217", "366");
+                        }
+                        else
+                        {
+                            Player.WalkToPoint("678", "310");
+                        }
+                        break;
+                    default:
+                        Player.WalkToPoint("485", "351");
+                        break;
+                }
+            }
+            catch { }
+        }
+    }
 }
