8176d9a9cd5af8b903711bc9f1c02fbb16cd7b83
Kuzyph <muhammadnajibakbar@outlook.com>
2025-09-11 23:42:10 +0800

    Auto atk delay to avoid combat while in other state

--- Grimoire/Botting/Commands/Combat/CmdQuestHunt.cs
+++ Grimoire/Botting/Commands/Combat/CmdQuestHunt.cs
@@ -5,6 +5,9 @@ using Grimoire.Tools;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using Grimoire.UI;
+using System.Web.UI;
+using System;
+using System.Linq;
 
 namespace Grimoire.Botting.Commands.Combat
 {
@@ -14,9 +17,9 @@ namespace Grimoire.Botting.Commands.Combat
         public string Cell { get; set; }
         public string Pad { get; set; }
         public string Monster { get; set; }
-        public string ItemName { get; set; }
-        public ItemType ItemType { get; set; }
-        public string Quantity { get; set; }
+        public string _ItemName { get; set; }
+        public ItemType _ItemType { get; set; }
+        public string _Quantity { get; set; }
         public Game.Data.Quest Quest
         {
             get;
@@ -28,13 +31,14 @@ namespace Grimoire.Botting.Commands.Combat
 
         public async Task Execute(IBotEngine instance)
         {
-            string[] items = ItemName.Split(',');
-            if (ItemType == ItemType.Items)
+            string[] items = _ItemName.Split(',');
+            string[] quantities = _Quantity.Split(',');
+            if (_ItemType == ItemType.Items)
             {
                 return;
             }
 
-            if (!Player.Map.Equals(Map.Split('-')[0]))
+            while (!Player.Map.Equals(Map.Split('-')[0]))
             {
                 CmdJoin join = new CmdJoin
                 {
@@ -47,28 +51,33 @@ namespace Grimoire.Botting.Commands.Combat
                 {
                     string[] safeCell = ClientConfig.GetValue(ClientConfig.C_SAFE_CELL).Split(',');
                     Player.MoveToCell(safeCell[0], safeCell[1]);
-                    await instance.WaitUntil(() => Player.CurrentState != Player.State.InCombat, timeout:3);
+                    await instance.WaitUntil(() => Player.CurrentState != Player.State.InCombat, timeout: 3);
                     await Task.Delay(1000);
                 }
 
                 await join.Execute(instance);
             }
             if (!Player.Cell.Equals(Cell)) Player.MoveToCell(Cell, Pad);
-            
+
             await Task.Delay(1000);
-
-
+            List<Monster> mon = World.GetAllMonsters();
             for (int i = 0; i < items.Length; i++)
             {
-
+                int qty = int.Parse(quantities[i]);
+                if (int.TryParse(items[i], out _))
+                {
+                    getMap(items[i], qty);
+                } else
+                {
+                    
+                }
             }
-
             CmdKillFor killFor = new CmdKillFor
             {
                 Monster = this.Monster,
-                ItemName = this.ItemName,
-                ItemType = this.ItemType,
-                Quantity = this.Quantity,
+                ItemName = this._ItemName,
+                ItemType = this._ItemType,
+                Quantity = this._Quantity,
                 DelayAfterKill = this.DelayAfterKill,
                 KillPriority = this.KillPriority,
             };
@@ -76,10 +85,28 @@ namespace Grimoire.Botting.Commands.Combat
             await killFor.Execute(instance);
         }
 
+        void getMap(string id, int qty = 1)
+        {
+            //List<TempItem> initialItems = Player.TempInventory.Items;
+            //TempItem newItem = null;
+
+            for (int i = -2; i < qty; i++) //extra 2 attempt for getting map anticipating failure
+            {
+                Player.GetMapItem(id);
+                Task.Delay(600);
+                //if (newItem != null)
+                //    continue;
+
+                //List<TempItem> newItems = Player.TempInventory.Items?.Except(initialItems ?? Enumerable.Empty<TempItem>()).ToList();
+                //newItem = newItems?.FirstOrDefault(x => x.Id == int.Parse(id)) ?? newItems?.FirstOrDefault();
+            }
+
+        }
+
         public override string ToString()
         {
-            string itemType = ItemType == ItemType.Items ? "Items" : "Temps";
-            return $"Hunt {itemType} {Quantity}x {ItemName}";
+            string itemType = _ItemType == ItemType.Items ? "Items" : "Temps";
+            return $"Hunt {itemType} {_Quantity}x {_ItemName}";
         }
 
     }
