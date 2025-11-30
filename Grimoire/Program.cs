0aee4041a54df1e96edf910899d2b4322073a85a
Kuzyph <muhammadnajibakbar@outlook.com>
2025-11-30 12:15:22 +0800

    v1.9

--- Grimoire/Program.cs
+++ Grimoire/Program.cs
@@ -11,8 +11,8 @@ namespace Grimoire
 {
 	internal static class Program
 	{
-		public static readonly string Version = "Li 1.8";
-		public static readonly string ReleaseDate = "14-11-2025";
+		public static readonly string Version = "Li 1.9";
+		public static readonly string ReleaseDate = "30-11-2025";
 		public static string PluginsPath { get; private set; }
 		public static Tools.Plugins.PluginManager PluginsManager { get; private set; }
 
